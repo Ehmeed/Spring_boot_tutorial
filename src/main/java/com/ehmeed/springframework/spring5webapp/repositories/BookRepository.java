ecb281266bc83daa1129f08d915150bd60e8fc03
Ehmeed <milanhruban@seznam.cz>
2017-11-12 23:47:44 +0100

    Lesson 12 bootstrap

--- src/main/java/com/ehmeed/springframework/spring5webapp/repositories/BookRepository.java
+++ src/main/java/com/ehmeed/springframework/spring5webapp/repositories/BookRepository.java
@@ -3,5 +3,5 @@ package com.ehmeed.springframework.spring5webapp.repositories;
 import com.ehmeed.springframework.spring5webapp.model.Book;
 import org.springframework.data.repository.CrudRepository;
 
-public interface BookRepository extends CrudRepository<Book, Long>{
+public interface BookRepository extends CrudRepository<Book, Long> {
 }
