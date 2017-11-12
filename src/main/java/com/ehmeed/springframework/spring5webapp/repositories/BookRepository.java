fbcf4a936e4f79d9dd76b4d5b7e1d352e47fe78e
Ehmeed <milanhruban@seznam.cz>
2017-11-12 23:34:50 +0100

    Lesson 11 repositories

---
+++ src/main/java/com/ehmeed/springframework/spring5webapp/repositories/BookRepository.java
@@ -0,0 +1,7 @@
+package com.ehmeed.springframework.spring5webapp.repositories;
+
+import com.ehmeed.springframework.spring5webapp.model.Book;
+import org.springframework.data.repository.CrudRepository;
+
+public interface BookRepository extends CrudRepository<Book, Long>{
+}
